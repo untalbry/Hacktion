e5bdebc78e84b0cf84b3bdfb67c51035bef419ec
bryancsinformatica <bryancsinformatica@gmail.com>
2024-01-29 15:17:20 -0600

    add(for loop for get i pokemon to notion)

--- src/index.js
+++ src/index.js
@@ -9,19 +9,22 @@ const notion = new Client({auth: process.env.NOTION_KEY})
 const pokeArray = [] 
 
 async function getPokemon(){
-    await axios.get('https://pokeapi.co/api/v2/pokemon/2')
-    .then((poke) => {
-       const pokeData = {
-            "name" : poke.data.name,
-            "id" : poke.data.id,
-            "hp" : poke.data.stats[0].base_stat
-       }
-       pokeArray.push(pokeData)
-       console.log(`Fetching ${pokeData.name} from PokeAPI.`)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+    for(let i = 1; i<= 10 ; i++){
+        await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            .then((poke) => {
+                const pokeData = {
+                        "name" : poke.data.name,
+                        "id" : poke.data.id,
+                        "hp" : poke.data.stats[0].base_stat
+                }
+                pokeArray.push(pokeData)
+                console.log(`Fetching ${pokeData.name} from PokeAPI.`)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+    
     createNotionPage()
 }
 
@@ -29,7 +32,6 @@ getPokemon()
 
 async function createNotionPage(){
     for(let pokemon of pokeArray){
-        console.log(process.env.NOTION_KEY)
         console.log("Sending data to notion. . .")
         const response = await notion.pages.create({
             "parent": {
